8bf78acec85d45783a04c46481f7be1e81385f0e
ERICK-DANIEL <erickdanielmartinezmartinez123@gmail.com>
2026-02-15 20:41:47 -0600

    Remove tools, Spotify & Terms from footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -21,21 +21,11 @@ export default function Footer() {
         <span>Soundtrics</span>
       </div>
 
-      <div className={styles.tools}>
-        <span>{dict.footer.tools.title}</span>
-        <a href={`/${lang}/charts`}>{dict.footer.tools.myCharts}</a>
-        <a href="/delete-duplicates">{dict.footer.tools.duplicateRemover}</a>
-      </div>
-
       <div className={styles.links}>
         <span>{dict.footer.links.title}</span>
         <a href="">
           {dict.footer.links.twitter}
           <i className="bi bi-twitter-x"></i>
-        </a>
-        <a href="">
-          {dict.footer.links.spotify}
-          <i className="bi bi-spotify"></i>
         </a>
         <a href="">
           {dict.footer.links.github}
@@ -47,7 +37,6 @@ export default function Footer() {
         <span>{dict.footer.resources.title}</span>
         <a href={`/${lang}/contact`}>{dict.footer.resources.contact}</a>
         <a href={`/${lang}/privacy`}>{dict.footer.resources.privacy}</a>
-        <a href={`/${lang}/terms`}>{dict.footer.resources.terms}</a>
       </div>
 
       <div className={styles.copyright}>
